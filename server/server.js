82ab98be20303c6cb7b397b138860d7bf6b53132
tekwin88 <limtekyong@gmail.com>
2016-11-09 10:26:33 +0800

    clean up files

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@ Meteor.startup(() => {
     });
 
     var users = Meteor.users.find().fetch();
-       _.each(users,function(userData){
-       if(userData.emails[0].address === 'cindy@abc.com'){
 
-           Roles.addUsersToRoles(userData,['admin']);
+    _.each(users,function(userData){
+       if(userData.emails[0].address === 'cindy@abc.com'){
+         Roles.addUsersToRoles(userData,['admin']);
        }
-   });
+    });
   }
 });
